d1524f25603c3140265e9391d76f29e257289b7b
K1TT3 <paulrokitte@gmail.com>
2022-12-11 19:01:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  //"X-Api-Key": "a2OGHdyD19BSWTAvzwLz9w==o4xI0wy0HpJhur5g"
+
+  const url2 =
+    "https://api.brightsky.dev/weather?lat=51&lon=12&date=2022-12-09";
+
+  //Methode für Geocoding API, Gib Name von Stadt, bekomme Lat und Lon,
+  //Lat und Lon für Ansprechen Brightsky API
+
+  const getCoordinate = () => {};
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url2).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder="Enter Location"
+          type="text"
+        />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Leipzig</p>
+          </div>
+          <div className="temp">
+            <h1>3°C</h1>
+          </div>
+          <div className="description">
+            <p>Wolkig</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p className="bold">2°C</p>
+            <p>gefühlt</p>
+          </div>
+          <div className="humidity">
+            <p className="bold">20%</p>
+            <p>Luftfeuchtigkeit</p>
+          </div>
+          <div className="wind">
+            <p className="bold">5 km/h</p>
+            <p>Windgeschwindigkeit</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
